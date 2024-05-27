9e6e66a46c3e9933745d1d7db3ee065c81f22cd4
OmerAqeel <omrraqeel@gmail.com>
2024-05-27 23:45:22 +0100

    initated the tokenizer, converted from text to sequences and encoding the string labels into integers

--- main.py
+++ main.py
@@ -12,3 +12,20 @@ from tensorflow.keras.layers import Embedding, Flatten, Dense  # these are the l
 data = pd.read_csv('datasets/train.txt', sep=";")
 data.columns = ["text", "Emotions"]  # set the column names
 print(data.head())  # print the first few rows of the data
+
+texts = data["text"].tolist()  # get the text data
+labels = data["Emotions"].tolist()  # get the label data
+
+# Tokenize the data
+tokenizer = Tokenizer() # create the tokenizer
+tokenizer.fit_on_texts(texts) # fit the tokenizer on the text
+
+sequences = tokenizer.texts_to_sequences(texts) # convert the text to sequences
+max_length = max([len(s.split()) for s in texts]) # get the max length of the sequences (the max number of words in a text) that will be used for padding
+padded_sequences = pad_sequences(sequences, maxlen=max_length) # pad the sequences
+
+# Encode the string labels into integers
+encoder = LabelEncoder() # create the encoder
+labels = encoder.fit_transform(labels) # fit the encoder on the labels
+
+
